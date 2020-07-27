4937e7a73d19862caed73e25b554f94a3bce2d56
cxl <1799359457@qq.com>
2020-07-27 22:03:57 +0800

    net

--- src/main/java/com/easyArch/net/SocketHandler.java
+++ src/main/java/com/easyArch/net/SocketHandler.java
@@ -13,24 +13,25 @@ import org.springframework.stereotype.Component;
 @Configuration
 @ChannelHandler.Sharable
 public class SocketHandler extends SimpleChannelInboundHandler<String> {
-    private static final Logger LOGGER= LoggerFactory.getLogger(SocketHandler.class);
+    private static final Logger logger= LoggerFactory.getLogger(SocketHandler.class);
     //@Autowired
     //这里就是连数据要的dao要注入
 
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, String o) {
-        LOGGER.info("收的数据"+o);
+        //这里就是要接的字符串o 你要自己解析split()一下
+        logger.info("收的数据"+o);
     }
     @Override
     public void channelActive(ChannelHandlerContext ctx) {
-        LOGGER.info("客户端加入连接:"+ctx.channel());
+        logger.info("客户端加入连接:"+ctx.channel());
     }
 
     @Override
     public void channelInactive(ChannelHandlerContext ctx) {
         Channel channel = ctx.channel();
         channel.closeFuture();
-        LOGGER.info("客户端断开连接："+ctx.channel());
+        logger.info("客户端断开连接："+ctx.channel());
     }
 
     @Override
