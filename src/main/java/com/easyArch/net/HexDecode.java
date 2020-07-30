7f7531705068b93766dd56740a537b46728f8a1b
cxl <1799359457@qq.com>
2020-07-30 19:42:20 +0800

    乱码解决办法

---
+++ src/main/java/com/easyArch/net/HexDecode.java
@@ -0,0 +1,20 @@
+package com.easyArch.net;
+
+import com.easyArch.util.HexUtil;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+import java.util.List;
+
+public class HexDecode extends ByteToMessageDecoder {
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+        byte[] bytes = new byte[in.readableBytes()];
+        in.readBytes(bytes);
+        String string = HexUtil.BinaryToHexString(bytes);
+        out.add(string);
+    }
+
+
+}
--- src/main/java/com/easyArch/net/Server.java
+++ src/main/java/com/easyArch/net/Server.java
@@ -24,7 +24,7 @@ public class Server {
                 NioEventLoopGroup boss = new NioEventLoopGroup();
                 NioEventLoopGroup work = new NioEventLoopGroup();
                 ServerBootstrap bootstrap = new ServerBootstrap();
-                bootstrap.group(boss, work).channel(NioServerSocketChannel.class).option(ChannelOption.SO_BACKLOG, 1024).option(ChannelOption.TCP_NODELAY, true).option(ChannelOption.SO_KEEPALIVE, true).childHandler(socketChannelInitializer);
+                bootstrap.group(boss, work).channel(NioServerSocketChannel.class).option(ChannelOption.SO_BACKLOG, 1024).option(ChannelOption.SO_KEEPALIVE, true).childHandler(socketChannelInitializer);
 
                 ChannelFuture future = bootstrap.bind(port).sync();
                 LOGGER.info("服务器启动");
